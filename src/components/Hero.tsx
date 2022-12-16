685f8a4e67840ad789078a2ad7dfd700f4403b0e
Juansubas <juansubas@gmail.com>
2022-12-16 00:39:50 -0500

    Finish BOX Employees

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {
   RiCheckboxBlankCircleFill,
+  RiCheckboxBlankFill,
   RiPlayFill,
   RiRestartFill,
   RiStarFill,
@@ -45,30 +46,40 @@ const Hero = () => {
             alt=""
             className="w-[450px] h-[450px] object-cover -mt-28"
           />
-          <div className="bg-white shadow-xl rounded-lg p-4 flex flex-col justify-center max-w-[250px] gap-2 mx-auto">
-            <img
-              src="https://img.freepik.com/foto-gratis/empresaria-confiada-sonriente-que-presenta-brazos-cruzados_1262-20950.jpg"
-              className="w-10 h-10 object-contain rounded-full"
-            />
-            <img
-              src="https://img.freepik.com/foto-gratis/hombre-barbudo-feliz-sorprendido-camisa-apuntando-lejos_171337-5021.jpg"
-              className="w-10 h-10 object-contain rounded-full"
-            />
-            <img
-              src="https://img.freepik.com/foto-gratis/retrato-hombre-afroamericano-inteligente-profesional-pie-manos-cruzadas-sobre-pecho-pose-confianza_176420-33861.jpg"
-              className="w-10 h-10 object-contain rounded-full"
-            />
-            <img
-              src="https://img.freepik.com/foto-gratis/feliz-joven_1098-20869.jpg"
-              className="w-10 h-10 object-contain rounded-full"
-            />
-            <img
-              src="https://img.freepik.com/foto-gratis/negocios-finanzas-empleo-concepto-mujeres-emprendedoras-exitosas-joven-empresaria-segura-anteojos-mostrando-gesto-pulgar-arriba-sostenga-computadora-portatil-garantice-mejor-calidad-servicio_1258-59118.jpg"
-              className="w-10 h-10 object-contain rounded-full"
-            />
-            <h2 className="text-xl font-medium traking-[1px] text-gray-800">120 + Employees</h2>
+          <div className="relative bg-white shadow-xl rounded-lg p-4 flex flex-col justify-center max-w-[250px] gap-2 mx-auto">
+            <div className="flex items-center">
+              <img
+                src="https://img.freepik.com/foto-gratis/empresaria-confiada-sonriente-que-presenta-brazos-cruzados_1262-20950.jpg"
+                className="w-10 h-10 object-cover rounded-full ring-2 ring-gray-300 bg-white"
+              />
+              <img
+                src="https://img.freepik.com/foto-gratis/hombre-barbudo-feliz-sorprendido-camisa-apuntando-lejos_171337-5021.jpg"
+                className="w-10 h-10 object-cover rounded-full ring-2 ring-gray-300 -ml-4 bg-white"
+              />
+              <img
+                src="https://img.freepik.com/foto-gratis/retrato-hombre-afroamericano-inteligente-profesional-pie-manos-cruzadas-sobre-pecho-pose-confianza_176420-33861.jpg"
+                className="w-10 h-10 object-cover rounded-full ring-2 ring-gray-300 -ml-4 bg-white"
+              />
+              <img
+                src="https://img.freepik.com/foto-gratis/feliz-joven_1098-20869.jpg"
+                className="w-10 h-10 object-cover rounded-full ring-2 ring-gray-300 -ml-4 bg-white"
+              />
+              <img
+                src="https://img.freepik.com/foto-gratis/negocios-finanzas-empleo-concepto-mujeres-emprendedoras-exitosas-joven-empresaria-segura-anteojos-mostrando-gesto-pulgar-arriba-sostenga-computadora-portatil-garantice-mejor-calidad-servicio_1258-59118.jpg"
+                className="w-10 h-10 object-cover rounded-full ring-2 ring-gray-300 -ml-4 bg-white"
+              />
+            </div>
+            <h2 className="text-xl font-medium traking-[1px] text-gray-800">
+              120 + Employees
+            </h2>
             <div className="flex items-center gap-2 text-lg text-gray-500">
-              <RiStarFill className="text-primary"/> 5.0 (3.1k Reviews)
+              <RiStarFill className="text-primary" /> 5.0 (3.1k Reviews)
+            </div>
+            <div className="absolute -right-12 -bottom-12 -z-10">
+              <div className="relative">
+                <RiCheckboxBlankCircleFill className=" text-primary text-8xl "/>
+                <RiCheckboxBlankFill className="absolute -left-7 -top-7 text-white text-8xl"/>
+              </div>
             </div>
           </div>
         </div>
